7f965a87669cc5255edf53c30388d1a1cb1b8518
Yuncun Shen <yuncuns@gmail.com>
2015-12-09 19:12:40 -0500

    Initial commit

---
+++ app/src/main/java/com/pipit/agc/agc/LandingFragment.java
@@ -0,0 +1,38 @@
+package com.pipit.agc.agc;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Eric on 12/8/2015.
+ */
+public class LandingFragment extends Fragment {
+
+    private static final String ARG_SECTION_NUMBER = "section_number";
+
+    /**
+     * Returns a new instance of this fragment for the given section
+     * number.
+     */
+    public static LandingFragment newInstance(int sectionNumber) {
+        LandingFragment fragment = new LandingFragment();
+        Bundle args = new Bundle();
+        args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public LandingFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.landing_fragment, container, false);
+        return rootView;
+    }
+
+}
